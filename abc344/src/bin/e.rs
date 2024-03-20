3348be8262544b50f71650cae2b2969a2fc668d7
toona-laptop <amanou.toona@gmail.com>
2024-03-20 21:46:13 +0900

    abc344 b, e: ac

--- abc344/src/bin/e.rs
+++ abc344/src/bin/e.rs
@@ -1,6 +1,7 @@
+use im_rc::HashMap;
+use itertools::Itertools;
 use proconio::input;
 
-const INFINITY: usize = 10usize.pow(10);
 #[allow(non_snake_case)]
 
 fn main() {
@@ -10,19 +11,53 @@ fn main() {
         Q: usize,
     }
 
-    let mut query = Vec::new();
+    let mut parent: HashMap<usize, usize> = HashMap::new();
+    let mut child: HashMap<usize, usize> = HashMap::new();
+
+    parent.insert(A[0], 0);
+    child.insert(0, A[0]);
+    child.insert(A[n - 1], usize::MAX);
+
+    for (&a1, &a2) in A.iter().zip(A[1..].iter()) {
+        parent.insert(a2, a1);
+        child.insert(a1, a2);
+    }
+
     for _ in 0..Q {
         input! {n: usize};
         if n == 1 {
             input! {
                 (x, y): (usize, usize),
             }
-            query.push((1, x, y));
+
+            child.insert(y, child[&x]);
+            parent.insert(child[&x], y);
+            parent.insert(y, x);
+            child.insert(x, y);
         } else {
             input! {
                 x: usize,
             }
-            query.push((2, x, 0));
+
+            let p = parent[&x];
+            let c = child[&x];
+
+            child.insert(p, c);
+            parent.insert(c, p);
         }
     }
+
+    let mut ans = vec![0];
+
+    loop {
+        let p = ans.last().unwrap();
+
+        if child[p] == usize::MAX {
+            break;
+        }
+        ans.push(child[p]);
+    }
+
+    let ans = ans.iter().skip(1).map(|x| x.to_string()).join(" ");
+    println!("{}", ans);
 }
