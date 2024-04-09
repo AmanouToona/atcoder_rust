4277319da59c2c0bd813b5d02e9a473bcb61b36a
toona <amanou.toona@gmail.com>
2024-04-09 22:25:52 +0900

    dp a, b: ac

--- dp/src/bin/b.rs
+++ dp/src/bin/b.rs
@@ -1,4 +1,23 @@
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (N, K): (usize, usize),
+        H: [usize; N],
+    }
+
+    let mut dp = vec![usize::MAX; N];
+
+    dp[0] = 0;
+
+    for i in 0..N - 1 {
+        for k in 1..=K {
+            let j = i + k;
+            if j >= N {
+                break;
+            }
+            dp[j] = dp[j].min(dp[i] + H[j].abs_diff(H[i]));
+        }
+    }
+    println!("{}", dp[N - 1]);
 }
