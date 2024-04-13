edd091ae7ea9043149065bc6c916f16b84c4629d
toona-laptop <amanou.toona@gmail.com>
2024-04-13 20:07:18 +0900

    typical90 008: ac

--- typical90/src/bin/008.rs
+++ typical90/src/bin/008.rs
@@ -1,4 +1,26 @@
+use ac_library::ModInt1000000007 as Mint;
 use proconio::input;
+use proconio::marker::Chars;
+use std::collections::HashMap;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        _: usize,
+        S:Chars,
+    }
+
+    let atcoder: Vec<char> = "atcoder".chars().collect();
+    let char2num: HashMap<char, usize> = atcoder.iter().enumerate().map(|(i, x)| (*x, i)).collect();
+
+    let mut dp = vec![Mint::new(0); atcoder.len() + 1];
+    dp[0] = Mint::new(1);
+
+    for s in S.iter() {
+        if let Some(&from) = char2num.get(s) {
+            let tmp = dp[from];
+            dp[from + 1] += tmp;
+        }
+    }
+
+    println!("{}", dp.last().unwrap());
 }
