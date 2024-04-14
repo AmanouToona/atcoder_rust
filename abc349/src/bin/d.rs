b6996d97e4201b43ffb7c6694dacbf20985bb68b
toona-laptop <amanou.toona@gmail.com>
2024-04-14 09:25:21 +0900

    refactor

--- abc349/src/bin/d.rs
+++ abc349/src/bin/d.rs
@@ -12,36 +12,13 @@ fn main() {
         if l == R {
             break;
         }
-
-        if l == 0 {
-            let mut wi = 0;
-            while (R - l) >= 2usize.pow(wi + 1) {
-                wi += 1;
-            }
-
-            ans.push(format!("{} {}", l, l + 2usize.pow(wi)));
-            l += 2usize.pow(wi);
-            continue;
+        let mut wi = 0;
+        while 2usize.pow(wi + 1) <= R - l && l % 2usize.pow(wi + 1) == 0 {
+            wi += 1;
         }
 
-        if l % 2 == 0 {
-            let mut wi = 0;
-            while 2usize.pow(wi + 1) <= l
-                && 2usize.pow(wi + 1) <= R - l
-                && l % 2usize.pow(wi + 1) == 0
-            {
-                wi += 1;
-            }
-
-            ans.push(format!("{} {}", l, l + 2usize.pow(wi)));
-            l += 2usize.pow(wi);
-            // println!("{}", wi);
-
-            continue;
-        }
-
-        ans.push(format!("{} {}", l, l + 1));
-        l += 1;
+        ans.push(format!("{} {}", l, l + 2usize.pow(wi)));
+        l += 2usize.pow(wi);
     }
 
     println!("{}", ans.len());
