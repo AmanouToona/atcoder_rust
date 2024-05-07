7ad39e29da540045146860a06ed8f07a64229cde
toona <amanou.toona@gmail.com>
2024-05-07 23:22:27 +0900

    typical90 022: AC

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -1,4 +1,13 @@
+use num::Integer;
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (A, B, C): (usize, usize, usize),
+    }
+
+    let D = A.gcd(&B).gcd(&C);
+
+    let ans = (A / D - 1) + (B / D - 1) + (C / D - 1);
+    println!("{}", ans);
 }
