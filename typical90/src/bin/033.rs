e802d3ac353c3a19ee99c71825cdd27d1a5ad2e2
toona <amanou.toona@gmail.com>
2024-05-13 20:59:58 +0900

    typical90 033: Ac

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -1,4 +1,15 @@
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (H, W): (usize, usize),
+    }
+
+    if H == 1 || W == 1 {
+        println!("{}", H * W);
+        return;
+    }
+
+    let ans = ((H + 1) / 2) * ((W + 1) / 2);
+    println!("{}", ans);
 }
