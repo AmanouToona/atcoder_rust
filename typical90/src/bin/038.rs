bdc701c36946f36bdcfb1f9bb8e747b017a77985
toona <amanou.toona@gmail.com>
2024-05-14 00:11:10 +0900

    typical90 038: AC

--- typical90/src/bin/038.rs
+++ typical90/src/bin/038.rs
@@ -1,4 +1,19 @@
+use num::Integer;
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (A, B): (i64, i64),
+    }
+
+    let gcd = A.gcd(&B);
+
+    if A / gcd > 10i64.pow(18u32) / B {
+        println!("Large");
+        return;
+    }
+
+    let ans = A.lcm(&B);
+
+    println!("{}", ans);
 }
