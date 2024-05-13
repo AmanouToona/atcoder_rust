a30cf71aa8798631e44c41893a2b884ce1ff37a3
toona <amanou.toona@gmail.com>
2024-05-13 20:26:36 +0900

    typical90 032: AC

--- typical90/src/bin/032.rs
+++ typical90/src/bin/032.rs
@@ -1,4 +1,45 @@
+use im_rc::HashSet;
+use itertools::Itertools;
 use proconio::input;
+use std::collections::HashMap;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        N: usize,
+        A: [[usize; N]; N],
+        M: usize,
+        XY: [(usize, usize); M],
+    }
+
+    let mut cant: HashMap<usize, HashSet<usize>> = HashMap::new();
+
+    for &(x, y) in XY.iter() {
+        let x = x - 1;
+        let y = y - 1;
+        cant.entry(x).or_insert(HashSet::new()).insert(y);
+        cant.entry(y).or_insert(HashSet::new()).insert(x);
+    }
+
+    let mut ans = usize::MAX;
+    'outer: for i in (0..N).permutations(N) {
+        // 噂を順守するか?
+        for (u, v) in i.iter().tuple_windows() {
+            if cant.entry(*u).or_default().contains(v) {
+                continue 'outer;
+            }
+        }
+
+        let mut cost = 0;
+        for (j, a) in i.iter().enumerate() {
+            cost += A[*a][j];
+        }
+
+        ans = ans.min(cost);
+    }
+
+    if ans == usize::MAX {
+        println!("-1");
+    } else {
+        println!("{}", ans);
+    }
 }
