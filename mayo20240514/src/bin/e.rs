eba090906a310e3d2273e8d4eeb8293c3ae601ee
toona <amanou.toona@gmail.com>
2024-05-15 00:35:21 +0900

    e : ac

--- mayo20240514/src/bin/e.rs
+++ mayo20240514/src/bin/e.rs
@@ -21,46 +21,42 @@ fn main() {
     // 傾き & 切片
     let mut cnt: HashMap<(i64, i64, i64), usize> = HashMap::new();
 
-    for pair in (0..N).permutations(2) {
+    for pair in (0..N).combinations(2) {
         let i = pair[0];
         let j = pair[1];
 
         // 傾き
-        let x = XY[j].0 - XY[i].0;
-        let y = XY[j].1 - XY[i].1;
+        let mut x = XY[j].0 - XY[i].0;
+        let mut y = XY[j].1 - XY[i].1;
+
+        if x < 0 {
+            x *= -1;
+            y *= -1;
+        }
+
+        if x == 0 {
+            y = 1;
+        }
 
         let gcd = x.gcd(&y);
 
-        let mut a1 = x / gcd;
-        let mut a2 = y / gcd;
-
-        if a1 < 0 {
-            a1 *= -1;
-            a2 *= -1;
-        }
-
-        if a1 == 0 && a2 < 0 {
-            a2 = 1;
-        }
-
-        if a2 == 0 {
-            a1 = 1;
-        }
+        x /= gcd;
+        y /= gcd;
 
         // y切片 的な物
         let x1 = XY[i].0;
         let y1 = XY[i].1;
 
-        let b = a1 * y1 - a2 * x1;
+        let b = x * y1 - y * x1;
 
-        *cnt.entry((a1, a2, b)).or_default() += 1;
+        *cnt.entry((x, y, b)).or_default() += 1;
     }
 
     // println!("{:?}", cnt);
 
     let mut ans = 0;
     for &v in cnt.values() {
-        if v > K * (K - 1) / 2 {
+        if v >= K * (K - 1) / 2 {
             ans += 1
         }
     }
