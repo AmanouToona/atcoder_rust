3dfe85e09724d4c018e9cd2004fe26889b68cf00
toona-laptop <amanou.toona@gmail.com>
2024-05-18 17:58:57 +0900

    typical90 048: AC

--- typical90/src/bin/048.rs
+++ typical90/src/bin/048.rs
@@ -1,4 +1,25 @@
+use std::collections::BinaryHeap;
+
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (N, K): (usize, usize),
+        AB: [(usize, usize); N],
+    }
+
+    let mut q = BinaryHeap::new();
+    for &(a, b) in AB.iter() {
+        q.push(b);
+        q.push(a - b);
+    }
+
+    let mut ans = 0;
+    for _ in 0..K {
+        if let Some(v) = q.pop() {
+            ans += v;
+        }
+    }
+
+    println!("{}", ans);
 }
