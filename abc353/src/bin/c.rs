7189d221326de79dc4210cca32537aa5a2af3d67
toona-laptop <amanou.toona@gmail.com>
2024-06-06 22:02:42 +0900

    ABC353 C: ac

--- abc353/src/bin/c.rs
+++ abc353/src/bin/c.rs
@@ -6,64 +6,47 @@ fn main() {
         A: [usize; N],
     }
 
-    let m: usize = 10usize.pow(8);
+    let MOD = 10usize.pow(8);
 
     let mut A = A;
     A.sort();
     let A = A;
 
-    let mut sub = vec![0];
-    for (i, &a) in A.iter().enumerate() {
-        if i == 0 {
-            continue;
-        }
-        sub.push(A[i] - A[i - 1]);
-    }
-
-    for i in 0..(N - 1) {
-        sub[i + 1] += sub[i];
-    }
-
-    let mut cumsum = A.clone();
-    for i in 0..(cumsum.len() - 1) {
-        cumsum[i + 1] += cumsum[i];
+    let mut C = A.clone();
+    for i in 1..N {
+        C[i] += C[i - 1];
     }
 
     let mut ans = 0;
-    for (i, a) in A.iter().enumerate() {
-        if i == N - 1 {
-            break;
-        }
+    for (i, &a) in A.iter().take(N - 1).enumerate() {
+        ans += C[N - 1] - C[i];
+        ans += a * (N - 1 - i);
 
-        if A[N - 1] + a < m {
-            ans += cumsum[N - 1] - cumsum[i];
-            ans += a * (N - 1 - i);
+        // 10 ** 8 を超えた数分だけ 10 ** 8 を引く
+        if A[N - 1] + a < MOD {
             continue;
         }
 
-        if A[i + 1] + a >= m {
-            let t = (A[i + 1] + a) / m;
-
-            ans += ()
+        if A[i + 1] + a >= MOD {
+            ans -= MOD * (N - 1 - i);
+            continue;
         }
 
-        // 2分探索で超える位置を特定
-        let mut left = i;
-        let mut right = N - 1;
+        let mut left = i + 1;
+        let mut right = N;
 
         while right - left > 1 {
             let mid = (right + left) / 2;
 
-            if A[mid] + a < m {
+            if A[mid] + a < MOD {
                 left = mid;
             } else {
                 right = mid;
             }
         }
 
-        ans += cumsum[right - 1] - cumsum[i];
-        ans += a * (right - i);
+        ans -= MOD * (N - 1 - left);
     }
 
-    println!("{:?}", sub);
+    println!("{}", ans);
 }
