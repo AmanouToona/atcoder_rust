f040d0b30f4c67ad44beb9eed0b4fd0cf46f5c12
toona <amanou.toona@gmail.com>
2024-06-09 00:13:44 +0900

    abc357 d: refactor

--- abc357/src/bin/d.rs
+++ abc357/src/bin/d.rs
@@ -13,24 +13,10 @@ fn main() {
         digit += 1;
     }
 
-    let mut conect = Mint::new(N);
-    let mut ans = Mint::new(0);
-    let mut ans_digit = Mint::new(1);
+    let n_10 = Mint::new(10usize.pow(digit));
 
-    let mut times = Mint::new(10usize.pow(digit));
-    let mut times_digit = Mint::new(digit);
-
-    for i in 0..63 {
-        if N >> i & 1 == 1 {
-            // ans += conect * 10usize.pow(ans_digit);  みたいなことをしたい
-            ans += conect * ans_digit;
-            ans_digit *= times;
-        }
-
-        conect += conect * times;
-        times_digit *= 2;
-        times *= times;
-    }
+    let ans =
+        Mint::new(N) * (n_10.pow(N.try_into().unwrap()) - 1) / Mint::new(10usize.pow(digit) - 1);
 
     println!("{}", ans);
 }
