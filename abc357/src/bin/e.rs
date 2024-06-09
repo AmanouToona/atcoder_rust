d38229b836d60960793125e3903983ade8b7c0d0
toona <amanou.toona@gmail.com>
2024-06-09 09:43:44 +0900

    abc357 e: solving

--- abc357/src/bin/e.rs
+++ abc357/src/bin/e.rs
@@ -1,23 +1,39 @@
 use ac_library::SccGraph;
-use im_rc::{HashMap, HashSet};
 use proconio::input;
+use std::collections::{HashMap, HashSet};
 
 struct Solve {
     g: Vec<HashSet<usize>>,
     gs: Vec<usize>,
-    n: usize,
+    ans: usize,
+    used: Vec<bool>,
 }
 
 impl Solve {
     fn new(g: Vec<HashSet<usize>>, gs: Vec<usize>) -> Self {
-        Solve { g, gs, n: 0 }
+        let n = g.len();
+        Solve {
+            g,
+            gs,
+            ans: 0,
+            used: vec![false; n],
+        }
     }
 
-    fn dfs(&mut self, u: usize, nc: usize) {
-        for v in self.g[u].iter() {
-            let add = self.gs[u];
-            let v = *v;
-            self.dfs(v, nc + add);
+    fn dfs(&mut self, u: usize, cnt: usize) {
+        if self.used[u] {
+            return;
+        }
+        self.used[u] = true;
+
+        let nexts = self.g[u].clone();
+        for v in nexts {
+            if self.used[v] {
+                continue;
+            }
+            self.ans += cnt * self.gs[v];
+
+            self.dfs(u, cnt + self.gs[u]);
         }
     }
 }
@@ -29,37 +45,34 @@ fn main() {
         A: [usize; N],
     }
 
-    let mut g = SccGraph::new(N);
+    let mut graph = SccGraph::new(N);
     for (i, &a) in A.iter().enumerate() {
-        g.add_edge(i, a - 1);
+        graph.add_edge(i, a - 1);
     }
 
-    let group = g.scc();
+    let group = graph.scc();
 
-    let mut n2group = HashMap::new();
-    for (i, g_) in group.iter().enumerate() {
-        for g__ in g_.iter() {
-            n2group.insert(g__, i);
-        }
-        println!("{:?}", g_);
-    }
-
-    let mut g = vec![HashSet::new(); group.len()];
-    for g_ in group.iter() {
-        for g2 in g_.iter() {
-            let u = n2group.get(g2).unwrap();
-            let v = n2group.get(&(A[*g2] - 1)).unwrap();
-            if u == v {
-                continue;
-            }
-            g[*u].insert(v);
+    let mut node2group = HashMap::new();
+    for (i, g) in group.iter().enumerate() {
+        for node in g.iter() {
+            node2group.insert(*node, i);
         }
     }
 
-    let g_cnt: Vec<usize> = group.iter().map(|x| x.len()).collect();
+    let mut gg = vec![HashSet::new(); group.len()]; // group graph
+    for (i, &a) in A.iter().enumerate() {
+        let &u = node2group.get(&i).unwrap(); // ここが違う 寝る
+        let &v = node2group.get(&(a - 1)).unwrap();
+        gg[u].insert(v);
+    }
 
-    // let mut g = vec![Vec::new(); N];
-    // for (i, &a) in A.iter().enumerate() {
-    //     g[i].push(a - 1);
-    // }
+    let mut gs: Vec<usize> = group.iter().map(|x| x.len()).collect(); // group size
+
+    let mut sol = Solve::new(gg, gs);
+
+    for u in 0..group.len() {
+        sol.dfs(u, 0);
+    }
+
+    println!("{}", sol.ans);
 }
