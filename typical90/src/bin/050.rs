a781bc60fdcdcb1c2bbfd30c827c5fa855346e8f
toona <amanou.toona@gmail.com>
2024-06-27 22:52:15 +0900

    typical90 050: ac

--- typical90/src/bin/050.rs
+++ typical90/src/bin/050.rs
@@ -1,4 +1,25 @@
+use ac_library::ModInt1000000007 as Mint;
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (N, L): (usize, usize),
+    }
+
+    let mut dp = vec![Mint::new(0); N + 1];
+    dp[0] += 1;
+
+    'outer: for i in 0..=N {
+        for &d in [1, L].iter() {
+            let j = i + d;
+            if j > N {
+                continue 'outer;
+            }
+
+            let t = dp[i];
+            dp[j] += t;
+        }
+    }
+
+    println!("{}", dp[N]);
 }
