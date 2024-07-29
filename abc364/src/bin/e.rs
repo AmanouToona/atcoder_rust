e9bdc5a90a3a465fc0a46820de163b25974cb1af
toona <amanou.toona@gmail.com>
2024-07-29 22:49:16 +0900

    abc364 e: 1WA

--- abc364/src/bin/e.rs
+++ abc364/src/bin/e.rs
@@ -1,23 +1,49 @@
+use std::usize;
+
 use proconio::input;
 #[allow(non_snake_case)]
 fn main() {
     input! {
         (N, X, Y): (usize, usize, usize),
-        AB: [(usize, usize); N],
+    mut     AB: [(usize, usize); N],
     }
+    AB.sort_by(|x, y| y.1.cmp(&x.1));
+    AB.sort();
+
+    println!("{:?}", AB);
 
     // dp [i個目までみた][合計の甘さ sa][食べた個数 m] = 合計のしょっぱさの最小値 sb
-    let mut dp = vec![vec![vec![usize::max; N + 1]; X + 1]; N + 1];
+    let mut dp = vec![vec![vec![usize::MAX; N + 1]; X + 2]; N + 1];
     dp[0][0][0] = 0;
 
     // 配る dp
-    for i in 0..N {
+    for (i, &(a, b)) in AB.iter().enumerate() {
         for sa in 0..=X {
             for m in 0..=i {
-                for &(a, b) in AB.iter() {
-                    let nxt_a = (sa + a).min(X);
+                // 食べない
+                dp[i + 1][sa][m] = dp[i + 1][sa][m].min(dp[i][sa][m]);
+
+                // 食べる
+                if dp[i][sa][m] > Y {
+                    continue;
+                }
+
+                let nxt_a = (sa + a).min(X + 1);
+                dp[i + 1][nxt_a][m + 1] = dp[i + 1][nxt_a][m + 1].min(dp[i][sa][m] + b);
+            }
+        }
+    }
+
+    // 答え
+    let mut ans = 0;
+    for i in 1..=N {
+        for sa in 0..=(X + 1) {
+            for m in 1..=i {
+                if dp[i][sa][m] < usize::MAX {
+                    ans = ans.max(m);
                 }
             }
         }
     }
+    println!("{ans}")
 }
