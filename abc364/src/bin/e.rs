45df6758deb82a807eb0feacd1066c82c1d8b860
toona <amanou.toona@gmail.com>
2024-07-31 20:22:02 +0900

    abc364 e: ac

--- abc364/src/bin/e.rs
+++ abc364/src/bin/e.rs
@@ -7,10 +7,6 @@ fn main() {
         (N, X, Y): (usize, usize, usize),
     mut     AB: [(usize, usize); N],
     }
-    AB.sort_by(|x, y| y.1.cmp(&x.1));
-    AB.sort();
-
-    println!("{:?}", AB);
 
     // dp [i個目までみた][合計の甘さ sa][食べた個数 m] = 合計のしょっぱさの最小値 sb
     let mut dp = vec![vec![vec![usize::MAX; N + 1]; X + 2]; N + 1];
@@ -18,7 +14,7 @@ fn main() {
 
     // 配る dp
     for (i, &(a, b)) in AB.iter().enumerate() {
-        for sa in 0..=X {
+        for sa in 0..X {
             for m in 0..=i {
                 // 食べない
                 dp[i + 1][sa][m] = dp[i + 1][sa][m].min(dp[i][sa][m]);
@@ -36,14 +32,15 @@ fn main() {
 
     // 答え
     let mut ans = 0;
-    for i in 1..=N {
-        for sa in 0..=(X + 1) {
-            for m in 1..=i {
-                if dp[i][sa][m] < usize::MAX {
-                    ans = ans.max(m);
+    for i in 0..=N {
+        for a in 0..=X {
+            for m in 0..=i {
+                if dp[i][a][m] <= Y {
+                    ans = ans.max(m + 1);
                 }
             }
         }
     }
+    ans = ans.min(N);
     println!("{ans}")
 }
