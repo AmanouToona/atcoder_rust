9ba7614c44f559a641f2ab0f93f45faceea5b2ec
toona-laptop <amanou.toona@gmail.com>
2024-08-13 13:57:33 +0900

    abc366: wa & re

--- abc363/src/bin/e.rs
+++ abc363/src/bin/e.rs
@@ -1,4 +1,56 @@
+use itertools::iproduct;
 use proconio::input;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 #[allow(non_snake_case)]
 fn main() {
+    input! {
+        (H, W, Y): (usize, usize, usize),
+        A: [[usize; W]; H],
+    }
+
+    let mut q: BinaryHeap<(Reverse<usize>, usize, usize)> = BinaryHeap::new();
+    let mut used = vec![vec![false; W]; H];
+
+    // 外周部分を q に入れる
+    for (h, w) in iproduct!(0..H, 0..W) {
+        if h == 0 || h == H - 1 || w == 0 || w == W - 1 {
+            q.push((Reverse(A[h][w]), h, w));
+            used[h][w] = true;
+        }
+    }
+
+    let mut sink = vec![0; Y + 1];
+    let mut level = 0;
+    while let Some((Reverse(a), h, w)) = q.pop() {
+        level = level.max(a);
+        if level > Y {
+            break;
+        }
+        sink[level] += 1;
+
+        for (&dh, &dw) in [0, !0, 0, 1].iter().zip([1, 0, !0, 0].iter()) {
+            let vh = h.wrapping_add(dh);
+            let vw = w.wrapping_add(dw);
+
+            if vh >= H || vw >= W {
+                continue;
+            }
+
+            if used[vh][vw] {
+                continue;
+            }
+
+            q.push((Reverse(A[vh][vw]), vh, vw));
+            used[vh][vw] = true;
+        }
+    }
+
+    for i in 0..Y {
+        sink[i + 1] += sink[i];
+    }
+
+    for &s in sink.iter().skip(1) {
+        println!("{}", H * W - s);
+    }
 }
--- abc366/src/bin/e.rs
+++ abc366/src/bin/e.rs
@@ -1,10 +1,58 @@
-use std::collections::BTreeMap;
-
 use proconio::input;
+use std::collections::HashMap;
+
+#[allow(non_snake_case)]
+fn f(X: Vec<i32>) -> Vec<usize> {
+    let mut pos = HashMap::new();
+    for &x in X.iter() {
+        *pos.entry(x.clone()).or_default() += 1;
+    }
+
+    // 位置 -2 * 10^6 での Sigma |x - xi|
+    let band = 2 * 10_i32.pow(6);
+    let mut sum: i32 = 0;
+    for &x in X.iter() {
+        sum += x.abs_diff(-band) as i32;
+    }
+
+    // 位置を -2 * 10^6 -> 2 * 10^6 に操作しながら距離和を計算する
+    let mut over_i = X.len() as i32;
+    let mut under_i = 0;
+    let mut res = Vec::new();
+    for i in (-band)..(band) {
+        // 位置 i での距離の差を保存
+        res.push(sum.try_into().unwrap());
+
+        // 位置 i+ 1 での距離の差を計算する
+        over_i -= pos.get(&i).unwrap_or(&0);
+        under_i += pos.get(&i).unwrap_or(&0);
+        sum += -over_i + under_i;
+    }
+
+    res.sort();
+    res
+}
 
 #[allow(non_snake_case)]
 fn main() {
     input! {(N, D): (usize, usize),
-    mut XY: [(usize, usize);N],
+    mut XY: [(i32, i32);N],
     }
+
+    let X = XY.iter().map(|x| x.0).collect();
+    let X: Vec<usize> = f(X).into_iter().rev().collect();
+
+    let Y = XY.iter().map(|x| x.1).collect();
+    let Y = f(Y);
+
+    let mut ans = 0;
+    let mut right = 0; // 尺取り法用
+    for x in X.iter() {
+        while right < Y.len() && Y[right] + x <= D {
+            right += 1;
+        }
+        ans += right;
+    }
+
+    println!("{ans}");
 }
