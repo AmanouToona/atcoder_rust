a0e9847b5ff54e92ead2e62124b2fe198cdaf0cf
toona <amanou.toona@gmail.com>
2024-10-11 00:51:29 +0900

    abc310 e: refactor

--- abc310/src/bin/e.rs
+++ abc310/src/bin/e.rs
@@ -1,7 +1,6 @@
 use proconio::input;
 use proconio::marker::Chars;
-use std::collections::BTreeSet;
-use std::ops::Bound::{Excluded, Unbounded};
+use std::mem::swap;
 #[allow(non_snake_case)]
 fn main() {
     input! {
@@ -14,35 +13,19 @@ fn main() {
         .map(|x| if x == '0' { 0 } else { 1 })
         .collect();
 
-    let mut nand = vec![S[0]];
-    for &s in S.iter().skip(1) {
-        nand.push(1 - (nand.last().unwrap() & s));
-    }
+    let mut cnt_0: i64 = 0;
+    let mut cnt_1: i64 = 0;
+    let mut ans: i64 = 0;
 
-    let mut cumsum = vec![0];
-    for &i in nand.iter() {
-        cumsum.push(cumsum.last().unwrap() + i);
-    }
-
-    let mut pos_0 = BTreeSet::new();
-    for (i, &s) in S.iter().enumerate() {
+    for &s in S.iter() {
         if s == 0 {
-            pos_0.insert(i);
-        }
-    }
-    pos_0.insert(N);
-
-    let mut ans = 0;
-
-    for (i, (&s, &n)) in S.iter().zip(nand.iter()).enumerate() {
-        if s == n {
-            ans += cumsum.last().unwrap() - cumsum[i];
+            cnt_1 += cnt_0;
+            cnt_0 = 1;
         } else {
-            let &next_0 = pos_0.range((Excluded(i), Unbounded)).next().unwrap();
-
-            ans += cumsum.last().unwrap() - cumsum[next_0];
-            ans += next_0 - i - (cumsum[next_0] - cumsum[i]);
+            swap(&mut cnt_0, &mut cnt_1);
+            cnt_1 += 1;
         }
+        ans += cnt_1;
     }
 
     println!("{ans}");
