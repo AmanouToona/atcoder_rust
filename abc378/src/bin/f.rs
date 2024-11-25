c47e9733b3784a0cf663e0ad7d61da7083779d81
toona <amanou.toona@gmail.com>
2024-11-26 00:39:03 +0900

    abc378 e: ac

--- abc378/src/bin/f.rs
+++ abc378/src/bin/f.rs
@@ -1,57 +1,38 @@
 use proconio::input;
 
 struct Solve {
-    ans: usize,
     g: Vec<Vec<usize>>,
-    used: Vec<bool>,
-    dig: Vec<usize>,
+    dp: Vec<usize>, // 利用可能な次数2の頂点
+    ans: usize,
 }
 
 impl Solve {
-    fn new(g: Vec<Vec<usize>>, dig: Vec<usize>) -> Self {
+    fn new(g: Vec<Vec<usize>>) -> Self {
         let n = g.len();
         Solve {
+            g,
+            dp: vec![0; n],
             ans: 0,
-            g: g.clone(),
-            used: vec![false; n],
-            dig,
         }
     }
 
-    fn solve(&mut self, u: usize) -> usize {
-        self.ans = 0;
-
-        if self.dig[u] != 3 {
-            return 0;
-        }
-
-        self.dfs(u);
-
-        let ans = if self.ans == 0 {
-            0
-        } else {
-            (self.ans - 1) * self.ans / 2
-        };
-
-        ans
-    }
-
-    fn dfs(&mut self, u: usize) {
-        if self.used[u] == true {
-            return;
-        }
-
-        if self.dig[u] == 3 {
-            self.used[u] = true;
-        }
-
-        if self.dig[u] == 2 {
-            self.ans += 1;
-            return;
-        }
-
+    fn dfs(&mut self, u: usize, p: Option<usize>) {
         for &v in self.g[u].clone().iter() {
-            self.dfs(v);
+            if !p.is_none() && v == p.unwrap() {
+                continue;
+            }
+            self.dfs(v, Some(u));
+
+            if self.g[u].len() == 3 {
+                self.ans += self.dp[u] * self.dp[v];
+                self.dp[u] += self.dp[v];
+            } else if self.g[u].len() == 2 && self.g[v].len() == 3 {
+                self.ans += self.dp[v];
+            }
+        }
+
+        if self.g[u].len() == 2 {
+            self.dp[u] = 1;
         }
     }
 }
@@ -72,29 +53,10 @@ fn main() {
         g[v].push(u);
     }
 
-    let dig: Vec<usize> = g.iter().map(|x| x.len()).collect();
+    let mut solve = Solve::new(g);
 
-    let mut new_g = vec![vec![]; N];
-    for (u, &ref g1) in g.iter().enumerate() {
-        if g1.len() != 2 && g1.len() != 3 {
-            continue;
-        }
-        for v in g1.iter() {
-            if g[*v].len() != 2 && g[*v].len() != 3 {
-                continue;
-            }
-            new_g[u].push(*v);
-        }
-    }
-
-    let g = new_g;
-
-    let mut solve = Solve::new(g, dig);
-
-    let mut ans = 0;
-    for i in 0..N {
-        ans += solve.solve(i);
-    }
+    solve.dfs(0, None);
+    let ans = solve.ans;
 
     println!("{ans}");
 }
