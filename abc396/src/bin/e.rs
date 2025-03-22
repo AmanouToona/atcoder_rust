fdf77a4d93dd402fe8667122d55b47617223c3a0
toona <amanou.toona@gmail.com>
2025-03-22 20:56:29 +0900

    ac: e

--- abc396/src/bin/e.rs
+++ abc396/src/bin/e.rs
@@ -1,4 +1,7 @@
+use itertools::Itertools;
 use proconio::input;
+use std::collections::VecDeque;
+
 #[allow(non_snake_case)]
 fn main() {
     input! {
@@ -6,20 +9,95 @@ fn main() {
         XYZ: [(usize, usize, usize); M],
     }
 
-    let mut Z = vec![vec![!0; N]; N];
+    let mut g: Vec<Vec<(usize, usize)>> = vec![Vec::new(); N];
 
     for &(x, y, z) in XYZ.iter() {
         let x = x - 1;
         let y = y - 1;
 
-        if Z[x][y] != !0 {
-            println!("-1");
-            return;
-        }
-
-        Z[x][y] = z;
-        Z[y][x] = z;
+        g[x].push((y, z));
+        g[y].push((x, z));
     }
 
-    let mut A = vec![!0; N];
+    let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+
+    let mut A = vec![0; N];
+    for bit in 0..64 {
+        let mut a = vec![usize::MAX; N];
+
+        for u in 0..N {
+            let mut fp = VecDeque::new();
+            let mut cnt1 = 0;
+            let mut cnt0 = 0;
+
+            if a[u] != usize::MAX {
+                continue;
+            }
+            fp.push_back(u);
+            a[u] = 0;
+
+            cnt0 += 1;
+
+            for &(v, z) in g[u].iter() {
+                let av = a[u] ^ (z >> bit & 1);
+
+                if a[v] != usize::MAX && av != a[v] {
+                    println!("-1");
+                    return;
+                }
+
+                if a[v] != usize::MAX {
+                    continue;
+                }
+
+                a[v] = av;
+                q.push_back((v, z));
+                fp.push_back(v);
+
+                if av == 1 {
+                    cnt1 += 1;
+                } else {
+                    cnt0 += 1;
+                }
+            }
+
+            while let Some((u, _)) = q.pop_front() {
+                for &(v, z) in g[u].iter() {
+                    let av = a[u] ^ (z >> bit & 1);
+
+                    if a[v] != usize::MAX && av != a[v] {
+                        println!("-1");
+                        return;
+                    }
+
+                    if a[v] != usize::MAX {
+                        continue;
+                    }
+
+                    a[v] = av;
+
+                    if av == 1 {
+                        cnt1 += 1;
+                    } else {
+                        cnt0 += 1;
+                    }
+                    fp.push_back(v);
+                    q.push_back((v, z));
+                }
+            }
+
+            if cnt0 >= cnt1 {
+                while let Some(u) = fp.pop_front() {
+                    A[u] += a[u] << bit;
+                }
+            } else {
+                while let Some(u) = fp.pop_front() {
+                    A[u] += (1 - a[u]) << bit;
+                }
+            }
+        }
+    }
+
+    let ans: String = A.iter().join(" ");
+    println!("{ans}");
 }
