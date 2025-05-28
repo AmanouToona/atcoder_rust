a9fc7273db1d8d1755d4dbb49f449599ae5abe1a
toona <amanou.toona@gmail.com>
2025-05-28 20:27:15 +0900

    abc407 e: non self

--- abc407/src/bin/e.rs
+++ abc407/src/bin/e.rs
@@ -1,4 +1,30 @@
 use proconio::input;
+use std::collections::BinaryHeap;
+#[allow(non_snake_case)]
+fn solve() {
+    input! {N: usize}
+    input! {A: [usize; 2 * N]}
+
+    let mut q = BinaryHeap::new();
+    let mut ans = 0;
+
+    for (i, &a) in A.iter().enumerate() {
+        q.push(a);
+
+        if i % 2 == 1 {
+            continue;
+        }
+        ans += q.pop().unwrap();
+    }
+
+    println!("{ans}");
+}
+
 #[allow(non_snake_case)]
 fn main() {
+    input! {T: usize}
+
+    for _ in 0..T {
+        solve()
+    }
 }
